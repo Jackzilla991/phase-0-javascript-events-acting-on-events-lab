d6414d017b07743aea08a8b1ab91fe97f6e1288c
Jack Scarpelli <Festivetomcat99@gmail.com>
2023-01-04 19:43:41 -0500

    commit tree 887c82596db735a997720b48aa08b144b1bdb5be

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function moveDodgerRight() {
+    dodger = document.getElementById('dodger')
+    var left = dodger.style.left
+    left = parseInt(left)
+    dodger.style.left = (left + 10) + "px"
+  }
+  function moveDodgerLeft() {
+    dodger = document.getElementById('dodger')
+    var left = dodger.style.left
+    left = parseInt(left)
+    dodger.style.left = (left - 10) + "px"
+  }
